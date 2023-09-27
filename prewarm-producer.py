b2f9d7deaf13f2545e53cf35499249d05762e26c
Gino Filicetti <ginof@google.com>
2023-09-27 18:34:47 -0400

    removed old code

--- prewarm-producer.py
+++ prewarm-producer.py
@@ -1,18 +1,3 @@
-def read_ccloud_config(config_file):
-    conf = {}
-    with open(config_file) as fh:
-        for line in fh:
-            line = line.strip()
-            if len(line) != 0 and line[0] != "#":
-                parameter, value = line.strip().split('=', 1)
-                conf[parameter] = value.strip()
-    return conf
-
-from confluent_kafka import Producer
-producer = Producer(read_ccloud_config("client.properties"))
-producer.produce("my-topic", key="key", value="value")
-
-
 #!/usr/bin/env python
 
 import sys
