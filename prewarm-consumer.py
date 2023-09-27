2e57293719cd75521b3c61e594e6eea6686a67b2
Gino Filicetti <ginof@google.com>
2023-09-27 18:42:55 -0400

    first try at the consumer

--- prewarm-consumer.py
+++ prewarm-consumer.py
@@ -1,24 +1,3 @@
-from confluent_kafka import Consumer
-
-props = read_ccloud_config("client.properties")
-props["group.id"] = "python-group-1"
-props["auto.offset.reset"] = "earliest"
-
-consumer = Consumer(props)
-consumer.subscribe(["my-topic"])
-
-try:
-    while True:
-        msg = consumer.poll(1.0)
-        if msg is not None and msg.error() is None:
-            print("key = {key:12} value = {value:12}".format(key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
-except KeyboardInterrupt:
-    pass
-finally:
-    consumer.close()
-
-
-
 #!/usr/bin/env python
 
 import sys
@@ -51,7 +30,7 @@ if __name__ == '__main__':
             consumer.assign(partitions)
 
     # Subscribe to topic
-    topic = "purchases"
+    topic = "warming_urls"
     consumer.subscribe([topic], on_assign=reset_offset)
 
     # Poll for new messages from Kafka and print them.
@@ -67,8 +46,7 @@ if __name__ == '__main__':
                 print("ERROR: %s".format(msg.error()))
             else:
                 # Extract the (optional) key and value, and print.
-
-                print("Consumed event from topic {topic}: key = {key:12} value = {value:12}".format(
+                print("Consumed event from topic {topic}: key = {key:12} value = {value:22}".format(
                     topic=msg.topic(), key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
     except KeyboardInterrupt:
         pass
