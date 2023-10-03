b7ac52fa58a0f489e82a5e4249b7f257e5c4e26f
Gino Filicetti <ginof@google.com>
2023-10-03 16:08:28 -0400

    Real pubsub script this time

--- pubsub-setup.sh
+++ pubsub-setup.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-gcloud run deploy cdn-prewarm \
---image=us-central1-docker.pkg.dev/cdn-warming-poc/registry-docker/cdn-prewarm-container:latest \
---min-instances=1 \
---no-cpu-throttling \
---allow-unauthenticated \
---cpu-boost \
---region=us-central1
+gcloud pubsub topics create warming_urls
