e506ad65b299bd5558de91552fe53762c986d2b5
Gino Filicetti <ginof@google.com>
2023-10-03 17:16:55 -0400

    added more gcloud commands to set up pub sub invoking cloud run

--- pubsub-setup.sh
+++ pubsub-setup.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
+
+# create the topic we'll be using
 gcloud pubsub topics create warming_urls
+
+# create an SA specifically for calling CR upon new P/S messages
+gcloud iam service-accounts create cloud-run-pubsub-invoker \
+    --display-name "Cloud Run Pub/Sub Invoker"
+
+# give the new SA cloud run invoker privs
+gcloud run services add-iam-policy-binding cdn-warming-poc \
+    --member=serviceAccount:cloud-run-pubsub-invoker@cdn-warming-poc.iam.gserviceaccount.com \
+    --role=roles/run.invoker
+
+# give pub sub the account token creator privs
+gcloud projects add-iam-policy-binding cdn-warming-poc \
+   --member=serviceAccount:service-1034328539074@gcp-sa-pubsub.iam.gserviceaccount.com \
+   --role=roles/iam.serviceAccountTokenCreator
+
+gcloud pubsub subscriptions create us-west4 --topic warming_urls \
+    --ack-deadline=600 \
+    --push-endpoint=https://cdn-prewarm-pubsub-7amek6gxsq-uc.a.run.app/ \
+    --push-auth-service-account=cloud-run-pubsub-invoker@cdn-warming-poc.iam.gserviceaccount.com
+
+
