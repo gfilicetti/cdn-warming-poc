ef9682ae50e9c0bdc31f8e7f95cc72611f008d2d
Gino Filicetti <ginof@google.com>
2023-10-04 09:05:16 -0400

    moved the policy binding creation to the create-cloudrun script because it needs the name of the service and a new one will need to be created for every cloud run

--- scripts/create-cloud-run.sh
+++ scripts/create-cloud-run.sh
@@ -4,15 +4,24 @@
 REGION=${1:-"us-central1"}
 IMAGE=${2:-"us-central1-docker.pkg.dev/cdn-warming-poc/registry-docker/cdn-prewarm-container"}
 SERVICE=${3:-"cdn-prewarm-us-central1"}
+PROJECT=${4:-$(gcloud config get project)}
 
 printf "Using region: ${REGION} \n"
 printf "Using image: ${IMAGE} \n"
 printf "Using cloud run service: ${SERVICE} \n"
+printf "Using project: ${PROJECT} \n"
 
+# create the new cloud run service
 gcloud run deploy $SERVICE \
     --image=$IMAGE:latest \
     --min-instances=1 \
     --no-cpu-throttling \
     --allow-unauthenticated \
     --cpu-boost \
+    --region=$REGION
+
+# give the pub/sub invoker SA invoker privs on this new cloud run service
+gcloud run services add-iam-policy-binding $SERVICE \
+    --member=serviceAccount:cloudrun-pubsub-invoker@$PROJECT.iam.gserviceaccount.com \
+    --role=roles/run.invoker \
     --region=$REGION
--- scripts/pubsub-setup.sh
+++ scripts/pubsub-setup.sh
@@ -4,14 +4,10 @@
 
 TOPIC=${1:-"warming_urls"}
 PROJECT=${2:-$(gcloud config get project)}
-CLOUDRUN=${3:-"cdn-prewarm-us-central1"}
-REGION=${4:-"us-central1"}
 PROJECT_NUM=$(gcloud projects describe $PROJECT --format="value(projectNumber)")
 
 printf "Using topic: ${TOPIC} \n"
 printf "Using project: ${PROJECT} \n"
-printf "Using cloud run service: ${CLOUDRUN} \n"
-printf "Using policy region: ${REGION} \n"
 printf "Using project number: ${PROJECT_NUM} \n"
 
 # first create the topic
@@ -20,12 +16,6 @@ gcloud pubsub topics create $TOPIC
 # create an SA specifically for calling CR upon new P/S messages
 gcloud iam service-accounts create cloudrun-pubsub-invoker \
     --display-name "Cloud Run Pub/Sub Invoker"
-
-# give the new SA cloud run invoker privs
-gcloud run services add-iam-policy-binding $CLOUDRUN \
-    --member=serviceAccount:cloudrun-pubsub-invoker@$PROJECT.iam.gserviceaccount.com \
-    --role=roles/run.invoker \
-    --region=$REGION
 
 # give pub sub the account token creator privs
 gcloud projects add-iam-policy-binding $PROJECT \
