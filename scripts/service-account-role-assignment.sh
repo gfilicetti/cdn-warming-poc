3e2a135058fb0d58e5b3fc3e41b270b5c1f36003
Gino Filicetti <ginof@google.com>
2023-10-05 19:10:49 -0400

    Delete script that isn't needed

--- scripts/service-account-role-assignment.sh
+++
@@ -1,3 +0,0 @@
-gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT \
-    --member="serviceAccount:1034328539074-compute@developer.gserviceaccount.com" \
-    --role="roles/logging.logWriter"
