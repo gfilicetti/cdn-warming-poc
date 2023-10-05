dab17186af4df2f9fb4d952ac3974142ab547548
Gino Filicetti <ginof@google.com>
2023-10-05 19:05:42 -0400

    Added a description Clean up output text

--- src/cdn_prewarm/consumer-pubsub.py
+++ src/cdn_prewarm/consumer-pubsub.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python
+# This script will consume messages on a Pub/Sub subscription, extract the URL can call it, waiting for a 200
+# -- NOTE: This is meant to be run with gunicorn, typically in a python container, typically running in Cloud Run
 
 import requests
-import uuid
 import base64
-import socket
 from google.cloud import logging
 from flask import Flask, request
 from threading import Thread
@@ -13,20 +12,22 @@ from threading import Thread
 
 app = Flask(__name__)
 
+# Here we call the internal Google metadata server to get our instance id (NOTE: this code won't run outside of Google Cloud)
 def getInstanceId():
     response = requests.get('http://metadata.google.internal/computeMetadata/v1/instance/id', headers={"Metadata-Flavor": "Google"})
     return response.text
 
+# TODO: this is temporarily hard-coded, we need to figure out how to get parameters into a gunicorn server
+project='cdn-warming-poc-project'
+
+# This is a distinguishable id, unique to each container instance
 instance_id = getInstanceId()
 
 @app.route("/", methods=["POST"])
 def main():
 
-    # Create an ID for myself for logging purpose
-    my_id = uuid.uuid1()
-
     # Create Cloud Logging client
-    logging_client = logging.Client(project='cdn-warming-poc')
+    logging_client = logging.Client(project=project)
     logging_client.setup_logging()
     logger = logging_client.logger("cdn-warming-consumer-pubsub")
 
@@ -37,16 +38,16 @@ def main():
     if not envelope:
         msg = "No Pub/Sub message received"
         log_text = f"Bad Request: {msg}"
-        print(log_text)
         logger.log_text(log_text)
+        print(log_text)
         return (log_text, 400)
 
     # check if we have a message in the envelope
     if not isinstance(envelope, dict) or "message" not in envelope:
         msg = "Invalid Pub/Sub message format"
         log_text = f"Bad Request: {msg}"
-        print(log_text)
         logger.log_text(log_text)
+        print(log_text)
         return (log_text, 400)
 
     # pull the message from the envelope
@@ -59,14 +60,14 @@ def main():
         # make a get call to the url and capture the response
         response = requests.get(url)
 
-        log_text = f"{my_id} - Status: {response.status_code}; File: {response.json()['args']['file']}; Instance: {instance_id}"
-        print(log_text)
+        log_text = f"ID: {instance_id[-8:]} - Status: {response.status_code}; File: {response.json()['args']['file']}"
         logger.log_text(log_text)
+        print(log_text)
 
         return (log_text, 204)
     else:
         msg = "No data in the Pub/Sub message"
         log_text = f"Bad Request: {msg}"
-        print(log_text)
         logger.log_text(log_text)
+        print(log_text)
         return (log_text, 400)
