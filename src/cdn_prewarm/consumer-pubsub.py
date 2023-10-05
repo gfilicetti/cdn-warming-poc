484e7b661cb2cd41c11fcf2c3e7f351e2e73afd0
Gino Filicetti <ginof@google.com>
2023-10-05 14:14:42 -0400

    updated CR to print the instance id, unique to the container id

--- src/cdn_prewarm/consumer-pubsub.py
+++ src/cdn_prewarm/consumer-pubsub.py
@@ -22,8 +22,6 @@ instance_id = getInstanceId()
 @app.route("/", methods=["POST"])
 def main():
 
-    print(f"1. Top of main, instance: {instance_id}")
-
     # Create an ID for myself for logging purpose
     my_id = uuid.uuid1()
 
@@ -32,12 +30,10 @@ def main():
     logging_client.setup_logging()
     logger = logging_client.logger("cdn-warming-consumer-pubsub")
 
-    print("2. After constants")
-
     # Receive pub/sub message
     envelope = request.get_json()
-    print("3. After getting envelope")
 
+    # check if our envelope is real
     if not envelope:
         msg = "No Pub/Sub message received"
         log_text = f"Bad Request: {msg}"
@@ -45,6 +41,7 @@ def main():
         logger.log_text(log_text)
         return (log_text, 400)
 
+    # check if we have a message in the envelope
     if not isinstance(envelope, dict) or "message" not in envelope:
         msg = "Invalid Pub/Sub message format"
         log_text = f"Bad Request: {msg}"
@@ -52,24 +49,19 @@ def main():
         logger.log_text(log_text)
         return (log_text, 400)
 
+    # pull the message from the envelope
     pubsub_message = envelope["message"]
-    print(f"4. After getting message from envelope: {pubsub_message}")
 
     if isinstance(pubsub_message, dict) and "data" in pubsub_message:
         # get the url from the message
         url = base64.b64decode(pubsub_message["data"]).decode("utf-8").strip()
-        print(f"5. After getting url: {url}")
 
         # make a get call to the url and capture the response
-        print(f"6. Before making request")
         response = requests.get(url)
-        print(f"7. After getting response: {response}")
 
-        log_text = f"{my_id} - Status: {response.status_code}; File: {response.json()['args']['file']}"
+        log_text = f"{my_id} - Status: {response.status_code}; File: {response.json()['args']['file']}; Instance: {instance_id}"
         print(log_text)
         logger.log_text(log_text)
-
-        print(f"8. Before returning")
 
         return (log_text, 204)
     else:
